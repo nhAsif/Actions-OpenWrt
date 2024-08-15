8b97c79c8adc528c27d7c728720bfa1e5731f755
doyoman <63859866+doyoman@users.noreply.github.com>
2024-08-15 10:53:39 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,6 +13,9 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
+# ash -> bash
+sed -i 's|/bin/ash|/bin/bash|' package/base-files/files/etc/passwd
+
 # Modify default theme
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
