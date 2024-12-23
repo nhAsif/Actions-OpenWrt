51dcc4bee9fa42a2f65e9c76f075fa81f5bfbc16
doyoman <63859866+doyoman@users.noreply.github.com>
2024-12-23 14:41:32 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,19 +10,22 @@
 # See /LICENSE for more information.
 #
 
-# Modify default IP
+# 修改默认 IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/luci2/bin/config_generate
 
 # ash -> bash
 sed -i 's#/bin/ash#/bin/bash#' package/base-files/files/etc/passwd
 
-# Modify default theme
+# 修改默认主题
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
-# Modify hostname
+# 修改主机名
 sed -i 's/LEDE/OpenWrt/g' package/base-files/files/bin/config_generate
 sed -i 's/LEDE/OpenWrt/g' package/base-files/luci2/bin/config_generate
 
 # 修改 tailscale 菜单位置
 sed -i 's#admin/services#admin/vpn#g' feeds/luci/applications/luci-app-tailscale/root/usr/share/luci/menu.d/luci-app-tailscale.json
+
+# cloudflared 启动脚本增加 --protocol http2 参数，使国内连接更稳定
+sed -i '/procd_append_param command "--no-autoupdate"/a \\tprocd_append_param command "--protocol" "http2"' feeds/packages/net/cloudflared/files/cloudflared.init
